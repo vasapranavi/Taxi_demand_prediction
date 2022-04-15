285d41a61f5ebf4fa9dd0ae144b2e1c8de250195
Pranavi <vasapranavi@gmail.com>
2022-04-15 03:26:23 +0100

    added further unit tests

---
+++ tests/preprocessing/test_time_bin.py
@@ -0,0 +1,12 @@
+from demand_prediction.preprocessing import time_bin
+import unittest
+
+
+class Test(unittest.TestCase):
+    def test_bin_time(self):
+        '''tests whether the time binning works'''
+
+
+if __name__ == '__main__':
+    # begin the unittest.main()
+    unittest.main()
